1d6f4e4e23deaf0b3bc12f3f2dfc39ffdc8a67d5
Mark Oakley <QQOak>
2020-03-15 17:23:09 +0000

    Added wheel class

---
+++ HolonomicPrototype/HolonomicPrototype/Wheel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HolonomicPrototype
+{
+    public class Wheel
+    {
+        public int Angle { get; set; }
+        public int Radius { get; set; }
+
+
+
+    }
+}

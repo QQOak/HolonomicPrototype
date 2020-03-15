bfd1e41c8cf2f4ae3d15c1f26432f5bf06c35783
Mark Oakley <QQOak>
2020-03-15 21:56:41 +0000

    Joystick Helper now has direction and magnitude

--- HolonomicPrototype/HolonomicPrototype.Tests/JoystickHelperTests.cs
+++ HolonomicPrototype/HolonomicPrototype.Tests/JoystickHelperTests.cs
@@ -8,16 +8,34 @@ namespace HolonomicPrototype.Tests
     [TestClass]
     public class JoystickHelperTests
     {
-        [TestMethod]
-        public void GetDirection_FromJoyStickXYCoordinates()
+        [DataTestMethod]
+        [DataRow(0, 0, 0)]
+        [DataRow(0,90,90)]
+        [DataRow(90, 90, 45)]
+        [DataRow(-90,-90,-135)]
+        [DataRow(0,-90,-90)]
+        [DataRow(180, 0, 0)]
+        [DataRow(0,180,90)]
+        public void GetDirection_FromJoyStickXYCoordinates(int x, int y, double expected)
         {
-
+            double actual = JoystickHelper.GetAngleFromXYCoordinates(x, y);
+            Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void GetMagnitude_FromJoyStickXYCoordinates()
+        [DataTestMethod]
+        [DataRow(0,0,0)]
+        [DataRow(0,90,90)]
+        [DataRow(90,90,127.2792)]
+        [DataRow(-90,-90, 127.2792)]
+        [DataRow(0,-90,90)]
+        [DataRow(180,0,180)]
+        [DataRow(0,180,180)]
+        public void GetMagnitude_FromJoyStickXYCoordinates(int x, int y, double expected)
         {
+            double actual = JoystickHelper.GetMagnitudeFromXYCoordinate(x, y);
 
+            const double delta = 0.0001f;
+            Assert.AreEqual(expected, actual, delta);
         }
     }
 }
--- HolonomicPrototype/HolonomicPrototype/JoystickHelper.cs
+++ HolonomicPrototype/HolonomicPrototype/JoystickHelper.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace HolonomicPrototype
 {
-    public class JoystickHelper
+    public static class JoystickHelper
     {
+        public static double GetAngleFromXYCoordinates(int x, int y)
+        {
+            double radians = Math.Atan2(y, x);
+            double angle = radians * (180 / Math.PI);
+
+            return angle;
+        }
+
+        public static double GetMagnitudeFromXYCoordinate(int x, int y)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
     }
 }
